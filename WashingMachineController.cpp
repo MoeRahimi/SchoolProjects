67c4a32e7bf3986dad143796d490fdf9e20c435c
MoeRahimi <151080130+MoeRahimi@users.noreply.github.com>
2023-11-16 22:27:51 -0800

    Update WashingMachineController.cpp
    Added IO functions for the tub.

--- WashingMachineController.cpp
+++ WashingMachineController.cpp
@@ -7,19 +7,27 @@
 #include <gpio_library> //would be specific to microcontoller used
 #define LID_SENSOR 1 //would be the actual pin number on the microcontroller
 #define GUI_BACKLIGHT 2 //would be the actual pin or port on the microcontroller to tell the GUI to wake up
-#define GUI_INPUT 4 //would be the actual data port to the GUI display
+#define GUI_INPUT 3 //would be the actual data port to the GUI display
+#define TUB_STATE 4 //would be the port to read if the tub is full or empty, etc
+#define TUB_OUTPUT 5 //would be the port the tub accepts commands from the microcontroller
+
 void initializeLidSensor(){
     SerialPin(LID_SENSOR, INPUT, bool LidOpen)//pull sensor info and stores it in a bool for open or closed state
 }
 void GUIpower(){
     bool LightOn =! LidOpen; 
-    SerialPin(GUI_BACKLIGHT, OUTPUT, LightOn);//push the power state to the GUI display based on teh lid state
+    SerialPin(GUI_BACKLIGHT, OUTPUT, LightOn);//push the power state to the GUI display based on the lid state
 }
 char GUIinput(){
     SerialPin(GUI_INPUT, INPUT, char GUIselection); //read the button selection from the GUI
     return GUIselection;
 }
-
+void TubState(){
+    SerialPin(TUB_STATE, INPUT, char TubFull);//read when the tub is full or empty
+}
+void TubCommand(const string command){
+    SerialPin(TUB_OUTPUT, OUTPUT, command);//send commands to the tub module
+}
 
 int main() {
 

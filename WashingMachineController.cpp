12e62432dfd3058b523155c3047a001378f90bbc
MoeRahimi <151080130+MoeRahimi@users.noreply.github.com>
2023-11-16 16:05:32 -0800

    Create WashingMachineController.cpp
    Project for CSC 300

---
+++ WashingMachineController.cpp
@@ -0,0 +1,7 @@
+// Example of classes and obect interaction for group project in CSC 300. 
+//Authored by the group "Digital Destroyerz" composed of Moe Rahimi, Hugo Schroeder, Kamelish Candelaria Velez, Keith Askew.
+
+int main() {
+    
+    return 0;
+}
